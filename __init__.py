f701cadb8344313a7b35c40e5a93862835e163dd
David <david.schaefer@ufz.de>
2015-03-31 10:30:07 +0200

    added geogridfuncs functions

--- __init__.py
+++ __init__.py
@@ -2,8 +2,21 @@
 # -*- coding: utf-8 -*-
 
 from .src.geogrid import GeoGrid
+from .src.geogridfuncs import (
+    indexCoordinates,
+    coordinateIndex, 
+    addCells,     
+    removeCells,
+    enlargeGrid,
+    shrinkGrid,
+    mergeGrid,
+    maskGrid,
+    trimGrid,
+    snapGrid
+)
+    
 
-__all__ = ["GeoGrid"]
+__all__ = ["GeoGrid", shrinkGrid]
 
 # Delete src from package namespace -> no import geogrid.src 
 del src
