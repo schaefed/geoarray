15ed945a7997afdf540b1df2ae59e40c9609e669
David Schaefer <david.schaefe@gmail.com>
2015-07-15 23:19:13 +0200

    __init__.py: adapted to new structure

--- __init__.py
+++ __init__.py
@@ -1,22 +1,22 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from src.geogrid import GeoGrid
-from src.geogridfuncs import (
-    indexCoordinates,
-    coordinateIndex, 
-    addCells,     
-    removeCells,
-    enlargeGrid,
-    shrinkGrid,
-    mergeGrid,
-    maskGrid,
-    trimGrid,
-    snapGrid
-)
+from src.geogrid import fromfile, empty, full, zeros, ones, array
+# from src.geogridfuncs import (
+#     indexCoordinates,
+#     coordinateIndex, 
+#     addCells,     
+#     removeCells,
+#     enlargeGrid,
+#     shrinkGrid,
+#     mergeGrid,
+#     maskGrid,
+#     trimGrid,
+#     snapGrid
+# )
     
 
-__all__ = ["GeoGrid", shrinkGrid]
+__all__ = [fromfile, empty, full, zeros, ones, array]
 
 # Delete src from package namespace -> no import geogrid.src 
 del src
