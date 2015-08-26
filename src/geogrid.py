02900f43c9f5122d9db008bdf7a9ce4e1e1c0e80
David Schaefer <david.schaefed@ufz.de>
2015-08-26 12:14:38 +0200

    Adapted test structure to the new package layout without a seperate geogridfuncs.py

--- src/geogrid.py
+++ src/geogrid.py
@@ -4,18 +4,37 @@
 import re, os
 import gdal, osr
 import numpy as np
+from math import floor, ceil
 
-# needs to be extended
+MAX_PRECISION  = 10
+
+# could be extended, for available options see:
+# http://www.gdal.org/formats_list.html
 _DRIVER_DICT = {
     ".tif" : "GTiff",
     ".asc" : "AAIGrid",
-    ".bmp" : "BMP",
     ".img" : "HFA",
-    ".jpg" : "JPEG",
-    ".png" : "PNG"
 }
 
-_FILEREF = None
+# type mapping
+DTYPE2GDAL = {
+    "uint8": 1,
+    "int8": 1,
+    "uint16": 2,
+    "int16": 3,
+    "uint32": 4,
+    "int32": 5,
+    "float32": 6,
+    "float64": 7,
+    "complex64": 10,
+    "complex128": 11,
+}
+
+GDAL2DTYPE = {v:k for k,v in DTYPE2GDAL.items()}
+
+# The open gdal file objects need to outlive its _GeoGrid
+# instance. Therefore they are stored globally.
+_FILEREFS = []
 
 gdal.PushErrorHandler('CPLQuietErrorHandler')
 
@@ -66,24 +85,27 @@ def fromfile(fname):
         return dict(zip(proj_params[0::2],proj_params[1::2]))
 
     # the file object needs to survive 'data'
-    global _FILEREF
+    global _FILEREFS
+    
+    fobj = _openFile(fname)
+    _FILEREFS.append(fobj)
 
-    _FILEREF   = _openFile(fname)
+    rasterband = fobj.GetRasterBand(1)
+    geotrans   = fobj.GetGeoTransform()
 
-    rasterband = _FILEREF.GetRasterBand(1)
-    geotrans   = _FILEREF.GetGeoTransform()
+    nrows      = fobj.RasterYSize        
+    ncols      = fobj.RasterXSize
+    nbands     = fobj.RasterCount
 
-    nrows      = _FILEREF.RasterYSize        
-    ncols      = _FILEREF.RasterXSize
-    nbands     = _FILEREF.RasterCount
+    # dtype      = np.dtype(gdal.GetDataTypeName(rasterband.DataType))
+    dtype      = np.dtype(GDAL2DTYPE[rasterband.DataType])
 
-    dtype      = np.dtype(gdal.GetDataTypeName(rasterband.DataType))
-
-    data       = _FILEREF.GetVirtualMemArray(
+    data       = fobj.GetVirtualMemArray(
         gdal.GF_Write, cache_size = nbands*nrows*ncols*dtype.itemsize
     )
-    return _GeoArray(data,geotrans[3],geotrans[0],"ul",_cellsize(geotrans),
-                    rasterband.GetNoDataValue(),_projParams(_FILEREF))
+    return _GeoArray(data=data,yorigin=geotrans[3],xorigin=geotrans[0],
+                     origin="ul",nodata_value=rasterband.GetNoDataValue(),
+                    cellsize=_cellsize(geotrans),proj_params=_projParams(fobj))
 
 
 def tofile(fname,geogrid):
@@ -113,7 +135,7 @@ def tofile(fname,geogrid):
         driver = gdal.GetDriverByName("MEM")
         out = driver.Create(
             "",grid.ncols,grid.nrows,grid.nbands,
-            gdal.GetDataTypeByName(str(grid.dtype))
+            DTYPE2GDAL[str(grid.dtype)]
         )
         out.SetGeoTransform(
             (grid.xorigin, grid.cellsize,0,
@@ -122,8 +144,9 @@ def tofile(fname,geogrid):
         out.SetProjection(projection)
         for n in xrange(grid.nbands):
             band = out.GetRasterBand(n+1)
-            band.SetNoDataValue(float(grid.nodata_value)) 
-            band.WriteArray(grid[(n,Ellipsis) if grid.nbands > 1 else (Ellipsis)])
+            band.SetNoDataValue(float(grid.nodata_value))
+            banddata = grid[(n,Ellipsis) if grid.nbands > 1 else (Ellipsis)]
+            band.WriteArray(banddata)
         out.FlushCache()
         return out
             
@@ -133,7 +156,8 @@ def tofile(fname,geogrid):
     errormsg = gdal.GetLastErrorMsg()
     if errormsg or not out:
         raise IOError(errormsg)
-    
+
+        
 class _GeoArray(np.ndarray):
     
     """
@@ -176,8 +200,8 @@ class _GeoArray(np.ndarray):
         yopp = self.nrows * self.cellsize
         xopp = self.ncols * self.cellsize
         return { 
-            "ymax": self.yorigin if self.origin[0] == "u" else self.yorigin - yopp,
-            "ymin": self.yorigin if self.origin[0] == "l" else self.yorigin + yopp,
+            "ymin": self.yorigin if self.origin[0] == "l" else self.yorigin - yopp,
+            "ymax": self.yorigin if self.origin[0] == "u" else self.yorigin + yopp,
             "xmin": self.xorigin if self.origin[1] == "l" else self.xorigin - xopp,
             "xmax": self.xorigin if self.origin[1] == "r" else self.xorigin + xopp,
         }
@@ -227,7 +251,7 @@ class _GeoArray(np.ndarray):
         try:
             return self.shape[-3]
         except IndexError:
-            return 0
+            return 1
 
     @property
     def nrows(self):
@@ -243,62 +267,234 @@ class _GeoArray(np.ndarray):
         except IndexError:
             return 0
     
-    def write(self,fname):
-        tofile(fname,self)
-        # _GridWriter(self).write(fname)
+    def tofile(self,fname):
+        tofile(fname, self)
         
+    def indexCoordinates(self,y_idx,x_idx):
+        """
+        Input:
+            y_idx:  int/float
+            x_idx:  int/float
+        Output:
+            y_coor: int/float
+            x_coor: int/float
+        Purpose:
+            Returns the coordinates of the cell definied by the input indices.
+            Works as the reverse of getCoordinateIdx.
+        Note:
+            The arguments are expected to be numpy arrax indices, i.e. the 
+            upper left corner of the grid is the origin. 
+        """        
+        if (y_idx < 0 or x_idx < 0) or (y_idx >= self.nrows or x_idx >= self.ncols):
+            raise ValueError("Index out of bounds !")
+        yorigin, xorigin = self.getOrigin("ul")
+        y_coor =  yorigin - y_idx * self.cellsize        
+        x_coor =  xorigin + x_idx * self.cellsize
+        return y_coor, x_coor
+
+    def coordinateIndex(self,y_coor,x_coor):
+        """
+        Input:
+            y_coor: int/float
+            x_coor: int/float
+        Output:
+            y_idx:  int/float
+            x_idx:  int/float
+        Purpose:
+            Returns the indices of the cell in which the coordinates fall        
+        """
+        yorigin, xorigin = self.getOrigin("ul")
+
+        y_idx = int(floor((yorigin - y_coor)/float(self.cellsize))) 
+
+        x_idx = int(floor((x_coor - xorigin )/float(self.cellsize)))
+        if y_idx < 0 or y_idx >= self.nrows or x_idx < 0 or x_idx >= self.ncols:
+            raise ValueError("Given Coordinates not within the grid domain!")
+        return y_idx,x_idx
+
+    def trimGrid(self):
+        """
+        Input:
+            None
+        Output:
+            GeoGrid
+        Purpose:
+            Removes rows/cols containing only nodata values from the
+            margins of the GeoGrid Instance
+        """
+        y_idx, x_idx = np.where(self != self.nodata_value)
+        try:
+            return self.removeCells(
+                top=min(y_idx),bottom=self.nrows-max(y_idx)-1,
+                left=min(x_idx),right=self.ncols-max(x_idx)-1)
+        except ValueError:
+            return self
+
+    def removeCells(self,top=0,left=0,bottom=0,right=0):
+        """
+        Input:
+            top, left, bottom, right:    Integer
+        Output:
+            GeoGrid
+        Purpose:
+            Removes the number of given cells from the 
+            respective margin of the grid
+        Example:
+            top=1, left=0, bottom=1, right=2
+            fill_value  0
+
+                        |0|5|8|9| 
+                        ---------
+                        |1|2|0|0|         |1|2|
+                        ---------   -->   -----
+                        |3|4|4|1|         |3|4|
+                        ---------
+                        |0|0|0|1| 
+
+        """
+
+        top    = int(max(top,0))
+        left   = int(max(left,0))
+        bottom = int(max(bottom,0))
+        right  = int(max(right,0))
+
+        nrows = self.nrows - top  - bottom
+        ncols = self.ncols - left - right
+
+        yorigin, xorigin = self.getOrigin("ul")
+
+        return array(
+            data        = self[Ellipsis, top:top+nrows, left:left+ncols],
+            dtype       = self.dtype,
+            yorigin     = yorigin - top*self.cellsize ,
+            xorigin     = xorigin + left*self.cellsize,
+            origin      = "ul", 
+            nodata_value  = self.nodata_value,
+            cellsize    = self.cellsize,
+            proj_params = self.proj_params,
+        )
+
+    def shrinkGrid(self,bbox):
+        """
+        Input:
+            bbox: {"ymin": int/float, "ymax": int/float,
+                "xmin": int/float, "xmax": int/float}
+        Output:
+            GeoGrid
+        Purpose:
+            Shrinks the grid in a way that the given bbox is still 
+            within the grid domain.        
+        """
+        top    = floor(round((self.bbox["ymax"] - bbox["ymax"])
+                             /self.cellsize, MAX_PRECISION))
+        left   = floor(round((bbox["xmin"] - self.bbox["xmin"])
+                            /self.cellsize, MAX_PRECISION))
+        bottom = floor(round((bbox["ymin"] - self.bbox["ymin"])
+                            /self.cellsize, MAX_PRECISION))
+        right  = floor(round((self.bbox["xmax"] - bbox["xmax"])
+                            /self.cellsize, MAX_PRECISION))
+
+        return self.removeCells(max(top,0),max(left,0),max(bottom,0),max(right,0))        
+
+    def addCells(self,top=0,left=0,bottom=0,right=0):
+        """
+        Input:
+            top:    int 
+            left:   int 
+            bottom: int 
+            right:  int 
+        Output:
+            GeoGrid
+        Purpose:
+            Adds the number of given cells to the respective
+            margin of the grid.
+        Example:
+            top=1, left=0, bottom=1, right=2
+            fill_value  0
+
+                                      |0|0|0|0| 
+                                      ---------
+                        |1|2|         |1|2|0|0|    
+                        ------   -->  ---------
+                        |4|3|         |3|4|0|0|
+                                      ---------
+                                      |0|0|0|0| 
+        """
+
+        top    = int(max(top,0))
+        left   = int(max(left,0))
+        bottom = int(max(bottom,0))
+        right  = int(max(right,0))
+
+        shape = list(self.shape)
+        shape[-2:] = self.nrows + top  + bottom, self.ncols + left + right
+        yorigin,xorigin = self.getOrigin("ul")
+
+        out = empty(
+            shape        = shape,
+            dtype        = self.dtype,        
+            yorigin      = yorigin + top*self.cellsize ,
+            xorigin      = xorigin - left*self.cellsize,
+            origin       = "ul",
+            nodata_value = self.nodata_value,
+            cellsize     = self.cellsize,
+            proj_params  = self.proj_params,
+        )
+
+        # the Ellipsis ensures that the function works with 
+        # arrays with more than two dimensions
+        out[Ellipsis, top:top+self.nrows, left:left+self.ncols] = self
+        return out
+
+    def enlargeGrid(self,bbox):
+        """
+        Input:
+            bbox: {"ymin": int/float, "ymax": int/float,
+                "xmin": int/float, "xmax": int/float}
+        Output:
+            GeoGrid
+        Purpose:
+            Enlarges the GeoGrid Instance in a way that the given bbox will 
+            be part of the grid domain. Added rows/cols ar filled with
+            the grid's nodata value
+        """
+        top    = ceil(round((bbox["ymax"] - self.bbox["ymax"])
+                            /self.cellsize,MAX_PRECISION))
+        left   = ceil(round((self.bbox["xmin"] - bbox["xmin"])
+                            /self.cellsize,MAX_PRECISION))
+        bottom = ceil(round((self.bbox["ymin"] - bbox["ymin"])
+                            /self.cellsize,MAX_PRECISION))
+        right  = ceil(round((bbox["xmax"] - self.bbox["xmax"])
+                            /self.cellsize,MAX_PRECISION))
+        return self.addCells(max(top,0),max(left,0),max(bottom,0),max(right,0))        
+
+    def snapGrid(self,target):
+        """
+        Input:
+            GeoGrid
+        Output:
+            None
+        Purpose:
+            Shift the grid origin in a way that it matches the nearest corner
+            of any gridcell in target
+        Restrictions:
+            The shift will only alter the grid coordinates.
+            No changes to the data will be done. In case of large
+            shifts the physical integrety of the data might
+            be disturbed!
+        """
+
+        diff = np.array(self.getOrigin()) - np.array(target.getOrigin(self.origin))
+        dy,dx = abs(diff)%target.cellsize * np.sign(diff)
+
+        if abs(dx) > self.cellsize/2.:
+            dx += self.cellsize
+
+        if abs(dy) > self.cellsize/2.:
+            dy += self.cellsize
+
+        self.xorigin -= dx
+        self.yorigin -= dy
+
+
     nodata_value = property(fget = _getNodataValue, fset = _setNodataValue)
-
-    
-# class _GridWriter(object):
-
-#     def __init__(self,fobj):
-#         self.fobj = fobj
-        
-#     def _fnameExtension(self,fname):
-#         return os.path.splitext(fname)[-1].lower()
-
-#     def _getDriver(self,fext):
-#         if fext in _DRIVER_DICT:
-#             driver = gdal.GetDriverByName(_DRIVER_DICT[fext])
-#             metadata = driver.GetMetadata_Dict()
-#             if "YES" == metadata.get("DCAP_CREATE",metadata.get("DCAP_CREATECOPY")):
-#                 return driver
-#             raise IOError("Datatype canot be written")            
-#         raise IOError("No driver found for filenmae extension '{:}'".format(fext))
-
-#     def _proj4String(self):
-#         params = self.fobj.proj_params
-#         if params:
-#             return "+{:}".format(" +".join(
-#                 ["=".join(pp) for pp in params.items()])
-#                              )
-
-#     def _writeGdalMemory(self):
-#         driver = gdal.GetDriverByName("MEM")
-#         out = driver.Create(
-#             "",self.fobj.ncols,self.fobj.nrows,self.fobj.nbands,
-#             gdal.GetDataTypeByName(str(self.fobj.dtype))
-#         )
-#         out.SetGeoTransform(
-#             (self.fobj.xorigin, self.fobj.cellsize,0,
-#              self.fobj.yorigin, 0, self.fobj.cellsize)
-#         )
-#         srs = osr.SpatialReference()
-#         srs.ImportFromProj4(self._proj4String())
-#         out.SetProjection(srs.ExportToWkt())
-#         for n in xrange(self.fobj.nbands):
-#             band = out.GetRasterBand(n+1)
-#             band.SetNoDataValue(float(self.fobj.nodata_value)) 
-#             band.WriteArray(self.fobj[(n,Ellipsis) if self.fobj.nbands > 1 else (Ellipsis)])
-#         out.FlushCache()
-#         return out
-
-#     def write(self,fname):
-#         memset = self._writeGdalMemory()
-#         fext = self._fnameExtension(fname)        
-#         outdriver = self._getDriver(fext)
-#         out = outdriver.CreateCopy(fname,memset,0)
-#         errormsg = gdal.GetLastErrorMsg()
-#         if errormsg or not out:
-#             raise IOError(errormsg)
