b6d9300e2015f96e4da6abafca8d71beaf4a06ee
David Schaefer <david.schaefe@gmail.com>
2015-11-16 21:42:32 +0100

    added simple setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name       = "geoarray",
+    version    = "1.0",
+    author     = "David Schaefer",
+    url        = "https://github.com/schaefed/geoarray",
+    py_modules = ["geoarray"],
+)
