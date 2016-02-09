048e59306099e5a5235c485e2f45da4ed5768091
David Schaefer <david.schaefe@gmail.com>
2016-02-09 15:19:20 +0100

    restructed repo

---
+++ geoarray/__init__.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from geoarray import (
+    array,
+    zeros,
+    ones,
+    full,
+    empty,
+    zeros_like,
+    ones_like,
+    full_like,
+    empty_like,
+    fromfile,
+    _DRIVER_DICT
+)
